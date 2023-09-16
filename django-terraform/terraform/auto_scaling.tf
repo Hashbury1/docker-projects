95862ef70520918d49a2b92d6f48bc83567a6466
hashbury Env <efefiomarchibong@gmail.com>
2023-09-16 05:11:24 +0100

    update variable and tfstate

--- django-terraform/terraform/auto_scaling.tf
+++ django-terraform/terraform/auto_scaling.tf
@@ -3,8 +3,9 @@ resource "aws_autoscaling_group" "ecs-cluster" {
   min_size             = var.autoscale_min
   max_size             = var.autoscale_max
   desired_capacity     = var.autoscale_desired
-  wait_for_capacity_timeout = 300
   health_check_type    = "EC2"
   launch_configuration = aws_launch_configuration.ecs.name
   vpc_zone_identifier  = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
 }
+
+
